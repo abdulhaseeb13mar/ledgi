f29f6dcd40147b684afe34a528ccd6b8c1ce8de5
abdulhaseeb13mar <abdulhaseeb13mar@gmail.com>
2026-03-26 12:54:41 +0500

    feat: update firebase configuration to use environment variables

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -2,22 +2,13 @@ import { getApp, getApps, initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
-// const firebaseConfig = {
-//   apiKey: "AIzaSyADDC5Ur7FbB8MRMcGmXUY0fBJqFuyoz_g",
-//   authDomain: "khaata-ledger.firebaseapp.com",
-//   projectId: "khaata-ledger",
-//   storageBucket: "khaata-ledger.firebasestorage.app",
-//   messagingSenderId: "573556950304",
-//   appId: "1:573556950304:web:8c00c24b38ab3124414345",
-// };
-
 const firebaseConfig = {
-  apiKey: "AIzaSyB1Mu37BOFEl3GIy9bOmPtnUju5kxRntNI",
-  authDomain: "kamel-hisaab.firebaseapp.com",
-  projectId: "kamel-hisaab",
-  storageBucket: "kamel-hisaab.firebasestorage.app",
-  messagingSenderId: "577518418175",
-  appId: "1:577518418175:web:f51dba5a56c0cb068634ca",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
 const app = getApps().length ? getApp() : initializeApp(firebaseConfig);
